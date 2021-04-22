329c8b32fed9ecee292d23898a385126ade1f145
carlospgraciano <carlosfollows@gmail.com>
2021-04-22 19:56:27 -0400

    Initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>
+    <p>Made with the &#10084;&#65039; from Sarah Drasner course.</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ store/index.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+  fooddata: []
+});
+
+// export const getters = {
+//   getterValue: state => {
+//     return state.value
+//   }
+// }
+
+export const mutations = {
+  updateFoodData: (state, data) => {
+    state.fooddata = data;
+  }
+};
+
+export const actions = {
+  async getFoodData({ state, commit }) {
+    if (state.fooddata.length) return;
+    try {
+      const data = await fetch(
+        "https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": process.env.AWS_API_KEY
+          }
+        }
+      ).then(response => response.json());
+      console.log(data);
+      commit("updateFoodData", data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
